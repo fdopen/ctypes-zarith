fbc5416a3a8e5764858922546233d96566f6b411
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2020-05-11 22:50:28 -0700

    adding support for mpq

--- lib/ctypes_zarith.mli
+++ lib/ctypes_zarith.mli
@@ -1,12 +1,33 @@
-type t
+module MPZ : sig
 
-val make : unit -> t Ctypes.abstract Ctypes.ptr
-(** like {!Ctypes.make}, but with finalise and type already specified *)
+  type t
 
-val of_z : Z.t -> t Ctypes.abstract Ctypes.ptr
+  val make : unit -> t Ctypes.abstract Ctypes.ptr
+  (** like {!Ctypes.make}, but with finalise and type already specified *)
 
-val to_z : t Ctypes.abstract Ctypes.ptr -> Z.t
+  val of_z : Z.t -> t Ctypes.abstract Ctypes.ptr
 
-val mpz_srcptr : Z.t Ctypes.typ
+  val to_z : t Ctypes.abstract Ctypes.ptr -> Z.t
 
-val mpz_ptr : t Ctypes.abstract Ctypes.ptr Ctypes.typ
+  val zarith : Z.t Ctypes.typ
+
+  val t_ptr : t Ctypes.abstract Ctypes.ptr Ctypes.typ
+
+end
+
+module MPQ : sig
+
+  type t
+
+  val make : unit -> t Ctypes.abstract Ctypes.ptr
+  (** like {!Ctypes.make}, but with finalise and type already specified *)
+
+  val of_q : Q.t -> t Ctypes.abstract Ctypes.ptr
+
+  val to_q : t Ctypes.abstract Ctypes.ptr -> Q.t
+
+  val zarith : Q.t Ctypes.typ
+
+  val t_ptr : t Ctypes.abstract Ctypes.ptr Ctypes.typ
+
+end
