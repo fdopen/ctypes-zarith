7f07d8a446b4f6ded1481d722d96bb6becba344e
fdopen <andreashauptmann@t-online.de>
2020-05-13 18:49:32 +0200

    test stub generation

--- test/run_tests.ml
+++ test/run_tests.ml
@@ -1,19 +1,19 @@
-open Ctypes
 open Ctypes_zarith
 
-module Add = struct
-  let z_add =
-    Foreign.foreign "__gmpz_add"
-      (MPZ.t_ptr @-> MPZ.zarith @-> MPZ.zarith @-> returning void)
+module Add (X : sig
+  val z_add :
+    Ctypes_zarith.MPZ.t Ctypes.abstract Ctypes.ptr -> Z.t -> Z.t -> unit
+
+  val q_add :
+    Ctypes_zarith.MPQ.t Ctypes.abstract Ctypes.ptr -> Q.t -> Q.t -> unit
+end) =
+struct
+  open X
 
   let z_add a b =
     let r = MPZ.make () in
     z_add r a b;
     MPZ.to_z r
-
-  let q_add =
-    Foreign.foreign "__gmpq_add"
-      (MPQ.t_ptr @-> MPQ.zarith @-> MPQ.zarith @-> returning void)
 
   let q_add a b =
     let r = MPQ.make () in
@@ -27,7 +27,7 @@ module Add = struct
     let two = Z.of_int 2 in
     let three = Z.of_int 3 in
     let six = Z.of_int 6 in
-    let (/) n d = Q.make n d in
+    let ( / ) n d = Q.make n d in
     check (three = z_add one two);
     check (one / two = q_add (one / three) (one / six));
     let fz n =
@@ -46,7 +46,7 @@ module Add = struct
       let r = Q.(n + n) in
       check (r = q_add n n)
     in
-    let n = six / (Z.of_int max_int) in
+    let n = six / Z.of_int max_int in
     fq n;
     let n = Q.(n + n) in
     fq n;
@@ -55,9 +55,15 @@ module Add = struct
     ()
 end
 
+module Add_foreign = Add (Test_foreign)
+module Add_stubgen = Add (Test_stubgen)
+module Add_ppx = Add (Test_ppx)
+
 let () =
   Alcotest.run "ctypes-zarith"
     [
-      ("add", [ ("add", `Quick, Add.test_add) ]);
+      ("add (foreign)", [ ("add (foreign)", `Quick, Add_foreign.test_add) ]);
+      ("add (stubgen)", [ ("add (stubgen)", `Quick, Add_stubgen.test_add) ]);
+      ("add (ppx)", [ ("add (ppx)", `Quick, Add_ppx.test_add) ]);
       ("gc", [ ("gc", `Slow, Gc.compact) ]);
     ]
