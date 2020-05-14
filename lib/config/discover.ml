ed41d1ad4c4dad3fd692b6a83b33a22729185ac2
fdopen <andreashauptmann@t-online.de>
2020-05-14 14:09:46 +0200

    os specific default flags
    pkg-config support was only added recently to gmp.
    default values are taken from zarith's opam file.

--- lib/config/discover.ml
+++ lib/config/discover.ml
@@ -1,8 +1,26 @@
 module C = Configurator.V1
 
+let is_system op s =
+  let re = Str.regexp_string_case_fold op in
+  try
+    ignore (Str.search_forward re s 0 : int);
+    true
+  with Not_found -> false
+
 let () =
-  C.main ~name:"gmp" @@ fun c ->
-  let default = { C.Pkg_config.libs = [ "-lgmp" ]; cflags = [] } in
+  let sys = ref "" in
+  let args = [ ("-system", Arg.Set_string sys, "set system") ] in
+  C.main ~args ~name:"gmp" @@ fun c ->
+  let sys = !sys in
+  let libs, cflags =
+    if is_system "freebsd" sys || is_system "openbsd" sys then
+      ([ "-L/usr/local/lib"; "-lgmp" ], [ "-I/usr/local/include" ])
+    else if is_system "macosx" sys then
+      ( [ "-L/opt/local/lib"; "-L/usr/local/lib"; "-lgmp" ],
+        [ "-I/opt/local/include"; "-I/usr/local/include" ] )
+    else ([ "-lgmp" ], [])
+  in
+  let default = { C.Pkg_config.libs; cflags } in
   let conf =
     match C.Pkg_config.get c with
     | None -> default
